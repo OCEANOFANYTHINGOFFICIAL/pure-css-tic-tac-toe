49b71f922659059a103046bfb5085dda904e6bbc
oceanofanything <oceanofanything@gmail.com>
2021-04-30 20:16:14 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Look ma' no JS! :)
+
+/*
+I have seen demos of pure CSS Tic-tac-toe games, but I was unable to find a version that truly works. If I have missed it, please send me link, so I can see how another dev has created it.
+
+My version works as any normal Tic-tac-toe game would: 2 players can play against each other, it can result in either a win or a tie. Players can also restart the game.
+
+In my CodePen example, I used Jade and SASS, so that my code looks much cleaner and simpler to understand.
+*/
+// How it works: https://codepen.io/ziga-miklic/blog/pure-css-tic-tac-toe/
